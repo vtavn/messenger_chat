cbe1ceedae2c9f886ba04324a627481777064485
vtavn <vutienanh291@gmail.com>
2022-03-18 23:01:29 +0700

    [1] add static page

--- src/server.js
+++ src/server.js
@@ -1,27 +1,23 @@
 import express from 'express'
 import ConnectDB from './configs/connectDB'
-import ContactModel from './models/contact.model'
+import configViewEngine from './configs/viewEngine'
 
+// init app
 const app = express()
 //connect to mongodb
 ConnectDB()
 
-const hostname = "localhost"
-const port = 3300
+//config view engine 
+configViewEngine(app)
 
-app.get('/testdatabase', async (req, res) => {
-  try {
-    const item = {
-      userId: "1233123",
-      contactId: "12312312"
-    }
-    const contact = await ContactModel.createNew(item)
-    res.send(contact)
-  } catch (error) {
-    console.log(error)
-  }
+app.get('/', (req, res) => {
+  return res.render('main/master')
+})
+
+app.get('/login-register', (req, res) => {
+  return res.render('auth/loginRegister')
 })
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-  console.log(`Server is running at ${hostname}:${port}`)
+  console.log(`Server is running at ${process.env.APP_HOST}:${process.env.APP_PORT}`)
 })
